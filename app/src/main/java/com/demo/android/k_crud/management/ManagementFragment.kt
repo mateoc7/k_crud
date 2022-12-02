936007f3c42842049bad20ae9641a476da8bd6ad
Mateo Contreras <macontrerasc@ufpso.edu.co>
2022-12-02 07:43:32 -0500

    base app

---
+++ app/src/main/java/com/demo/android/k_crud/management/ManagementFragment.kt
@@ -0,0 +1,19 @@
+package com.demo.android.k_crud.management
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.demo.android.k_crud.R
+
+class ManagementFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_management, container, false)
+    }
+}
